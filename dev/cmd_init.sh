7a308b93847420e155c49329502e9f4dd9bcc82f
7db9a <repo.7db9a@gmail.com>
2021-02-24 19:37:24 -0500

    Saving progress: init with mnemonic.

--- dev/cmd_init.sh
+++ dev/cmd_init.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 dir_path="$1"
+mnemonic="$2"
+
 
 mkdir "$dir_path"
 mkdir "$dir_path"/files
 touch "$dir_path"/file-list.txt
 
 cd "$dir_path"
+echo "$mnemonic" | _imt_create_wallet_info_file_from_mnemonic
 git init
 cd  files
 git init
