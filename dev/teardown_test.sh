7d2052c4a66c487f73d137ec30e46c915aed8957
7db9a <repo.7db9a@gmail.com>
2021-02-25 22:06:20 -0500

    tests: pass teardown script test.

--- dev/teardown_test.sh
+++ dev/teardown_test.sh
@@ -5,7 +5,7 @@ testEquality() {
     ls immutag
     # Get exit codeo of ls: 2 means dir doesn't exist.
     result_exit_code=$(echo $?)
-    assertEquals "0" "$result_exit_code"
+    assertEquals "2" "$result_exit_code"
 }
 
 . shunit2
