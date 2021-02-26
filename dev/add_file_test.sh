cec0f52a061b5a12dd2ed28c90760312dd2dbc2e
7db9a <repo.7db9a@gmail.com>
2021-02-25 21:15:48 -0500

    tests(add): runs but fails.

--- dev/add_file_test.sh
+++ dev/add_file_test.sh
@@ -1,55 +1,15 @@
 #! /bin/sh
-# file: examples/equality_test.sh
 
-# test_file1.txt
-
-test_absolute_dir_path="$(</dev/stdin)"
-
-#echo ""
-#echo "path"
-#echo "$test_absolute_dir_path"
-#echo ""
-
-oneTimeSetUp() {
-	cd "$test_absolute_dir_path"
-	imt_init immutag "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
-
-	cd immutag
-
-	echo "foo" > foo.txt
-	echo "bar" > bar.txt
-	#echo "remake" > remake.txt
-	#echo "consign" > consign.txt
-	#echo "correct" > correct.txt
-	#echo "shear" > shear.txt
-	#echo "develop" > develop.txt
-	imt_add foo.txt test test1 test2 test3
-	imt_add bar.txt test test1 test2 test3
+oneTimeSetup() {
+	imt_add foo.txt tag1 tag2 tag3
+	imt_add bar.txt tag1 tag2 tag3
 }
 
 testEquality() {
-    cd "$test_absolute_dir_path"
-    cd immutag
     result_foo=$(eval cat files/1CaKbES6YZY2rm2grufw8gw1URafLdJN8Q)
     result_bar=$(eval cat files/17nZVxSmir9moZQSAwrPd7r7rRRdNqovGr)
     assertEquals 'foo' "$result_foo"
-    assertEquals 'bar' "$result_bar"
-}
-
-# Test complaines it can't remove files because they don't exist, but test passes.
-oneTimeTearDown() {
-	cd "$test_absolute_dir_path"
-	rm -r immutag
-
-	#rm file-list.txt \
-	#foo.txt \
-	#bar.txt
-	##remake.txt
-	##consign.txt
-	##correct.txt
-	##shear.txt
-	##develop.txt
-	#rm -r files/
+    assertEquals 'ba' "$result_bar"
 }
 
 . shunit2
