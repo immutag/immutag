df9bb0a42d6b2eb1e7e1e7e64b7060ce8c6e59e1
7db9a <repo.7db9a@gmail.com>
2021-02-27 14:20:07 -0500

    tests: add tag replace tests (fail).

---
+++ dev/tag_replace_setup_test.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+oneTimeSetUp() {
+    imt_init immutag "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
+
+    echo "foo" > immutag/foo.txt
+    echo "bar" > immutag/bar.txt
+
+    mv tag_replace_test.sh immutag/
+    mv add_tag_test.sh immutag/
+}
+
+testEquality() {
+    result_foo=$(eval cat immutag/foo.txt)
+    result_bar=$(eval cat immutag/bar.txt)
+    result_test=$(eval ls immutag/tag_replace_test.sh)
+    result_test2=$(eval ls immutag/add_tag_test.sh)
+    assertEquals "foo" "$result_foo"
+    assertEquals "bar" "$result_bar"
+    assertEquals "immutag/tag_replace_test.sh" "$result_test"
+    assertEquals "immutag/add_tag_test.sh" "$result_test2"
+}
+
+# Test complaines it can't remove files because they don't exist, but test passes.
+#oneTimeTearDown() {
+#    rm -r immutag
+#    #rm file-list.txt \
+#    #foo.txt \
+#    #bar.txt
+#    ##remake.txt
+#    ##consign.txt
+#    ##correct.txt
+#    ##shear.txt
+#    ##develop.txt
+#    #rm -r files/
+#}
+
+. shunit2
+# 1 1CaKbES6YZY2rm2grufw8gw1URafLdJN8Q foo txt test test1 test2 test3
+# 2 17nZVxSmir9moZQSAwrPd7r7rRRdNqovGr bar txt test test1 test2 test3
+# 3 15QRcwb52ZGbFLgfwSc6zWgBqEdCtLqMWg crunch csv test test1 test2 test3
+# 4 1FuAYSe8N3K2x6tCnaCgzLZExVAtDkMHWt test_file1 txt test test1 test2
