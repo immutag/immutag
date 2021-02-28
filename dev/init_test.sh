98c7206c87a860e6b195ea90ef407863f430dfd1
7db9a <repo.7db9a@gmail.com>
2021-02-27 22:53:42 -0500

    docs: add code comment on how to use init cmd.

--- dev/init_test.sh
+++ dev/init_test.sh
@@ -5,6 +5,10 @@
 #	imt_add bar.txt tag1 tag2 tag3
 #}
 
+# To use command with cat
+# imt_init "$(eval cat $mnemonic)"
+# without the quotations, it fails.
+
 testEquality() {
     imt_init immutag "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
 
