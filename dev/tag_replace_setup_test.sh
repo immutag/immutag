5f77a7e4d05a1f53b7e58af25f967db860bee6ac
7db9a <repo.7db9a@gmail.com>
2021-03-01 15:52:08 -0500

    tests: pass tag replace test.

--- dev/tag_replace_setup_test.sh
+++ dev/tag_replace_setup_test.sh
@@ -1,24 +1,19 @@
 #! /bin/sh
 
 oneTimeSetUp() {
-    imt_init immutag "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
+    imt_init main "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
 
-    echo "foo" > immutag/foo.txt
-    echo "bar" > immutag/bar.txt
-
-    mv tag_replace_test.sh immutag/
-    mv add_tag_test.sh immutag/
+    echo "foo" > foo.txt
+    echo "bar" > bar.txt
 }
 
 testEquality() {
-    result_foo=$(eval cat immutag/foo.txt)
-    result_bar=$(eval cat immutag/bar.txt)
-    result_test=$(eval ls immutag/tag_replace_test.sh)
-    result_test2=$(eval ls immutag/add_tag_test.sh)
+    result_foo=$(eval cat foo.txt)
+    result_bar=$(eval cat bar.txt)
+    result_test=$(eval ls add_tag_test.sh)
     assertEquals "foo" "$result_foo"
     assertEquals "bar" "$result_bar"
-    assertEquals "immutag/tag_replace_test.sh" "$result_test"
-    assertEquals "immutag/add_tag_test.sh" "$result_test2"
+    assertEquals "add_tag_test.sh" "$result_test"
 }
 
 # Test complaines it can't remove files because they don't exist, but test passes.
