7e8c16133933324e395521824d1a9a689990de62
7db9a <repo.7db9a@gmail.com>
2021-03-01 15:09:08 -0500

    tests: pass add tags test.

--- dev/add_tag_test.sh
+++ dev/add_tag_test.sh
@@ -12,8 +12,8 @@ testEquality() {
     imt_add "$name" foo.txt tag1 tag2 tag3
     imt_add "$name" bar.txt tag1 tag2 tag3
 
-    echo "1CaKbES6YZY2rm2grufw8gw1URafLdJN8Q" | imt_tag_add "$name"tag4 tag5 tag6 tag7
-    echo "17nZVxSmir9moZQSAwrPd7r7rRRdNqovGr" | imt_tag_add "$name"tag4 tag5
+    echo "1CaKbES6YZY2rm2grufw8gw1URafLdJN8Q" | imt_tag_add "$name" tag4 tag5 tag6 tag7
+    echo "17nZVxSmir9moZQSAwrPd7r7rRRdNqovGr" | imt_tag_add "$name" tag4 tag5
 
     cd "$immutag_path"
     cd "$name"
