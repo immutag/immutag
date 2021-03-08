203d057eb6df7f84a89c74f000560790dcdc2772
7db9a <repo.7db9a@gmail.com>
2021-03-08 16:45:36 -0500

    dev: get generation hash.

---
+++ dev/cmd_gen_hash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# $ add *name* file tags...
+
+name="$1"
+
+immutag_path="$HOME/immutag"
+
+cd "$immutag_path"
+cd "$name"
+
+tmp_file_name=$(eval cat file-list.txt | sha256sum | head -c 64)
+
+generation_sha256=$(eval cp store-addresses /tmp/immutag-state-"$tmp_file_name" && cat file-list.txt >> /tmp/immutag-state-"$tmp_file_name" | sha256sum | head -c 64)
+
+echo "$generation_sha256"
