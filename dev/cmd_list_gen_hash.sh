e053245067d6792ad28225ee24d27fd53d8c334c
7db9a <repo.7db9a@gmail.com>
2021-03-08 18:00:27 -0500

    List gens.

--- dev/cmd_list_gen_hash.sh
+++ dev/cmd_list_gen_hash.sh
@@ -15,10 +15,17 @@ done
 
 while :
 do
-        imt_rollforward "$name"
-	status=$(echo "$?")
 
-	if [ "$status" != "0" ];then
+        gitrev_oids_store_1=$(git rev-parse HEAD git-annex)
+        gitrev_oid_a_store_1=$(echo "$gitrev_oids_store_1" | sed -n '1p')
+        gitrev_oid_b_store_1=$(echo "$gitrev_oids_store_1" | sed -n '2p')
+        imt_rollforward "$name"
+        gitrev_oids_store_2=$(git rev-parse HEAD git-annex)
+        gitrev_oid_a_store_2=$(echo "$gitrev_oids_store_2" | sed -n '1p')
+        gitrev_oid_b_store_2=$(echo "$gitrev_oids_store_2" | sed -n '2p')
+
+	# Rolling forward in vain.
+	if [ "$gitrev_oids_store_1" = "$gitrev_oids_store_2" ];then
 		break
 	fi
 done
@@ -28,4 +35,4 @@ immutag_path="$HOME/immutag"
 cd "$immutag_path"
 cd "$name"
 
-imt_exit_checkout "$name"
+_imt_exit_checkout
