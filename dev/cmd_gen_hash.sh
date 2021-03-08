04b13931b2d0e45793fb35ef138c1aafb3d4ba25
7db9a <repo.7db9a@gmail.com>
2021-03-08 17:20:52 -0500

    Saving progress.

--- dev/cmd_gen_hash.sh
+++ dev/cmd_gen_hash.sh
@@ -12,7 +12,7 @@ cd "$name"
 get_gen() {
     tmp_file_name=$(eval cat file-list.txt | sha256sum | head -c 64)
 
-    generation_sha256=$(eval cp store-addresses /tmp/immutag-state-"$tmp_file_name" && cat file-list.txt >> /tmp/immutag-state-"$tmp_file_name" | sha256sum | head -c 64)
+    generation_sha256=$(eval cp store-addresses /tmp/immutag-state-"$tmp_file_name" && cat file-list.txt >> /tmp/immutag-state-"$tmp_file_name" | cat /tmp/immutag-state-"$tmp_file_name" | sha256sum | head -c 64)
 
     echo "$generation_sha256"
 }
