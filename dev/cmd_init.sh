690b78d8e6f483fbe5f4b0d300a66a0269d95563
7db9a <repo.7db9a@gmail.com>
2021-03-12 09:46:11 -0500

    refactor: changes per shellcheck.

--- dev/cmd_init.sh
+++ dev/cmd_init.sh
@@ -3,28 +3,28 @@
 dir_path="$1"
 mnemonic="$2"
 
-mkdir $HOME/immutag
+mkdir "$HOME"/immutag
 
-cd $HOME/immutag
+cd "$HOME"/immutag || exit
 
-mkdir "$dir_path"
+mkdir "$dir_path" || exit
 mkdir "$dir_path"/files
 echo "immutag version: 0.0.5" > "$dir_path"/file-list.txt
 echo "immutag software sha256: <sha256>" >> "$dir_path"/file-list.txt
 echo "" >> "$dir_path"/file-list.txt
 
-cd "$dir_path"
+cd "$dir_path" || exit
 echo "$mnemonic" | _imt_create_wallet_info_file_from_mnemonic
 git init
-cd  files
+cd  files || exit
 git init
 git annex init
 
 # Add new name to config
 
-in_config=$(rg --multiline "${dir_path}\n" $HOME/.immutag_config)
+in_config=$(rg --multiline "${dir_path}\n" "$HOME"/.immutag_config)
 if [ "$in_config" ]; then
     echo "$dir_path already exists"
 else
-    echo "$dir_path" >> $HOME/.immutag_config
+    echo "$dir_path" >> "$HOME"/.immutag_config
 fi
