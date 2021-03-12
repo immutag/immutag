1d882b33db9429f0cf4716a06f04a56336b0105c
7db9a <repo.7db9a@gmail.com>
2021-03-12 09:21:57 -0500

    refactor: double quote and other to prevent globbing.

--- dev/cmd_add.sh
+++ dev/cmd_add.sh
@@ -9,7 +9,7 @@ input="$@"
 name=$(echo "$input" | cut -d " " -f 1)
 
 file=$(echo "$input" | cut -d " " -f 2)
-file_abs_path=$(eval realpath $file)
+file_abs_path=$(eval realpath "$file")
 
 fullfilename="$file"
 filename=$(basename "$fullfilename")
@@ -22,8 +22,8 @@ metadata=$(echo "$fname" "$ext" "$tags")
 echo "$file"
 echo "$metadata"
 
-cd $immutag_path
-cd $name
+cd "$immutag_path" || exit
+cd "$name" || exit
 
 
 addr=$(eval _imt_print_list_only | _imt_new_index_addr | _imt_append_list "$metadata")
