240c7242a1e1e525073bf927751a3eb7401068a7
7db9a <repo.7db9a@gmail.com>
2021-03-12 09:48:03 -0500

    refactor: cmd commit changes per shellcheck.

--- dev/cmd_commit.sh
+++ dev/cmd_commit.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 name="$1"
-immutag_path="$HOME/immutag"
+immutag_path="$HOME"/immutag
 
-cd $immutag_path
-cd $name
+cd "$immutag_path" || exit
+cd "$name" || exit
 
 
 imt_commit_store
-_imt_update_list_store_hashes $name
+_imt_update_list_store_hashes "$name"
 imt_commit_list
 # Only does so if checked out from a rollback or rollforward.
 _imt_exit_checkout
