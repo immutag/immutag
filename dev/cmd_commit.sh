4b4a3baa9a1db1e90cd33c539ff2349d6253ce9e
7db9a <repo.7db9a@gmail.com>
2021-03-12 10:42:10 -0500

    refactor: supress output and error in commmit cmd.

--- dev/cmd_commit.sh
+++ dev/cmd_commit.sh
@@ -7,8 +7,8 @@ cd "$immutag_path" || exit
 cd "$name" || exit
 
 
-imt_commit_store
+imt_commit_store > /dev/null 2>&1
 _imt_update_list_store_hashes "$name"
-imt_commit_list
+imt_commit_list > /dev/null 2>&1
 # Only does so if checked out from a rollback or rollforward.
-_imt_exit_checkout
+_imt_exit_checkout > /dev/null 2>&1
