345870179aeb302021df5f4c283a12854fda764a
7db9a <repo.7db9a@gmail.com>
2021-03-15 13:25:55 -0400

    Saving progress: rollforward just in case before listing generations.

--- dev/cmd_list_gen_hash.sh
+++ dev/cmd_list_gen_hash.sh
@@ -4,6 +4,26 @@ name="$1"
 
 while :
 do
+
+        gitrev_oids_store_1=$(git rev-parse HEAD git-annex > /dev/null 2>&1)
+        gitrev_oid_a_store_1=$(echo "$gitrev_oids_store_1" | sed -n '1p')
+        gitrev_oid_b_store_1=$(echo "$gitrev_oids_store_1" | sed -n '2p')
+        imt_rollforward "$name" > /dev/null 2>&1
+	gitrev_oids_store_2=$(git rev-parse HEAD git-annex > /dev/null 2>&1)
+        gitrev_oid_a_store_2=$(echo "$gitrev_oids_store_2" | sed -n '1p')
+        gitrev_oid_b_store_2=$(echo "$gitrev_oids_store_2" | sed -n '2p')
+
+	# Rolling forward in vain.
+	if [ "$gitrev_oids_store_1" = "$gitrev_oids_store_2" ];then
+		break
+
+	fi
+done
+
+_imt_exit_checkout > /dev/null 2>&1
+
+while :
+do
 	imt_gen_hash "$name" 2> /dev/null
         imt_rollback "$name" > /dev/null 2>&1
 	status=$(echo "$?")
