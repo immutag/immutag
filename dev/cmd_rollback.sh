50e55b9399cb9731e3da94b12aac2c5cebb830a4
7db9a <repo.7db9a@gmail.com>
2021-03-17 16:53:05 -0400

    fix: only switch files gen if files actually changed.

--- dev/cmd_rollback.sh
+++ dev/cmd_rollback.sh
@@ -6,18 +6,24 @@ immutag_path="$HOME/immutag"
 cd $immutag_path
 cd $name
 
+store_oids_a=$(cat store-addresses | jq '.git_annex.addr')
+
+oid_a_1=$(echo "$store_oids_a" | cut -d ' ' -f 1 | sed 's/"//g')
+oid_a_2=$(echo "$store_oids" | cut -d ' ' -f 2 | sed 's/"//g')
+
 git checkout HEAD~1 > /dev/null 2>&1
 
-# Get git annex branch oids off of rolled back file list, and then checkout those out
-store_oids=$(cat store-addresses | jq '.git_annex.addr')
+# Get git annex branch oids off of rolled back file list, and then checkout those out if it's changed (maybe the tags changed and files are unchanged in previous gen).
+store_oids_b=$(cat store-addresses | jq '.git_annex.addr')
+if [ "$store_oids_a" != "$store_oids_b" ]; then
+    #oid_1_b=$(echo "$store_oids_b" | cut -d ' ' -f 1 | sed 's/"//g')
+    #oid_2_b=$(echo "$store_oids_b" | cut -d ' ' -f 2 | sed 's/"//g')
 
-oid_1=$(echo "$store_oids" | cut -d ' ' -f 1 | sed 's/"//g')
-oid_2=$(echo "$store_oids" | cut -d ' ' -f 2 | sed 's/"//g')
+    cd files/
 
-cd files/
-
-# We can't rollback the git-annex branch also, but if we do make the changes permanent, we can.
-git checkout HEAD~1 > /dev/null 2>&1
+    # We can't rollback the git-annex branch also, but if we do make the changes permanent, we can.
+    git checkout HEAD~1 > /dev/null 2>&1
+fi
 
 #echo "$oid_1"
 #echo "$oid_2"
--- dev/cmd_rollforward.sh
+++ dev/cmd_rollforward.sh
@@ -6,19 +6,21 @@ immutag_path="$HOME/immutag"
 cd $immutag_path
 cd $name
 
+# Get git annex branch oids before rollback.
+store_oids_a=$(cat store-addresses | jq '.git_annex.addr')
+#oid_a_1=$(echo "$store_oids_a" | cut -d ' ' -f 1 | sed 's/"//g')
+#oid_a_2=$(echo "$store_oids_a" | cut -d ' ' -f 2 | sed 's/"//g')
+
 git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout
 
-# Get git annex branch oids off of rolled back file list, and then checkout those out
-store_oids=$(cat store-addresses | jq '.git_annex.addr')
+store_oids_b=$(cat store-addresses | jq '.git_annex.addr')
+if [ "$store_oids_a" != "$store_oids_b" ]; then
+    cd files/
 
-oid_1=$(echo "$store_oids" | cut -d ' ' -f 1 | sed 's/"//g')
-oid_2=$(echo "$store_oids" | cut -d ' ' -f 2 | sed 's/"//g')
+    # We can't rollforward the git-annex branch also, but if we do make the changes permanent, we can.
+    git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout
 
-cd files/
-
-# We can't rollforward the git-annex branch also, but if we do make the changes permanent, we can.
-git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout
-
+fi
 #echo "$oid_1"
 #echo "$oid_2"
 
