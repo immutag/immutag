103f0e43e4824bfce5dcbdf7a03f34c1dbcdac36
7db9a <repo.7db9a@gmail.com>
2021-03-17 22:19:34 -0400

    dev: add command can optionally not add default file name and ext.

--- dev/imt.sh
+++ dev/imt.sh
@@ -9,13 +9,101 @@ if [ "$cmd" = "create" ];then
 	imt_init "$storename" "$mnemonic"
 fi
 
-if [ "$cmd" = "add" ];then
-        input="$*"
-	storename="$2"
-	filepath="$3"
-        tags=$(echo "$input" | cut -d " " -f 4-)
+#if [ "$cmd" = "add" ];then
+#        input="$*"
+#	storename="$2"
+#	filepath="$3"
+#        tags=$(echo "$input" | cut -d " " -f 4-)
+#
+#	imt_add "$storename" "$filepath" "$tags"
+#fi
 
-	imt_add "$storename" "$filepath" "$tags"
+if [ "$cmd" = "add" ];then
+
+    POSITIONAL=()
+    while [[ $# -gt 0 ]]
+    do
+    key="$1"
+
+    case $key in
+        --no-default-name)
+        NODEFAULTNAME=YES
+        shift # past argument
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+    done
+    set -- "${POSITIONAL[@]}" # restore positional parameters
+
+    if [ -n "${NODEFAULTNAME}" ];then
+        immutag_path="$HOME/immutag"
+
+        input="$*"
+
+        name="$2"
+	file="$3"
+        file_abs_path=$(eval realpath "$file")
+
+        # Exit script here if we can't find the file to be added to the store.
+        if [ ! -f "$file_abs_path" ];then
+            echo "Can't find file."
+            exit
+        fi
+
+        metadata=$(echo "$input" | cut -d " " -f 4-)
+
+        echo "add file: $file"
+        echo "tags: $metadata"
+
+        cd "$immutag_path" || exit
+        cd "$name" || exit
+
+
+        addr=$(eval _imt_print_list_only | _imt_new_index_addr | _imt_append_list "$metadata")
+
+        cp "$file_abs_path" files/"$addr"
+
+        _imt_commit "$name"
+    else
+        immutag_path="$HOME/immutag"
+
+        input="$*"
+
+        name="$2"
+	file="$3"
+        file_abs_path=$(eval realpath "$file")
+
+        # Exit script here if we can't find the file to be added to the store.
+        if [ ! -f "$file_abs_path" ];then
+            echo "Can't find file."
+            exit
+        fi
+
+        # To add file name and extension to tag.
+        fullfilename="$file"
+        filename=$(basename "$fullfilename")
+        fname="${filename%.*}"
+        ext="${filename##*.}"
+
+        tags=$(echo "$input" | cut -d " " -f 4-)
+        metadata=$(echo "$fname" "$ext" "$tags")
+
+        echo "add file: $file"
+        echo "tags: $metadata"
+
+        cd "$immutag_path" || exit
+        cd "$name" || exit
+
+
+        addr=$(eval _imt_print_list_only | _imt_new_index_addr | _imt_append_list "$metadata")
+
+        cp "$file_abs_path" files/"$addr"
+
+        _imt_commit "$name"
+    fi
 fi
 
 if [ "$cmd" = "add-tag" ];then
