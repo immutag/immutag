2a0ad9f0fd5d4ec7aa25d69ad9c6cd7d77ca7afd
7db9a <repo.7db9a@gmail.com>
2021-03-22 13:47:10 -0400

    dev: suppressed git checkout messages in roll operations.

--- dev/cmd_rollforward.sh
+++ dev/cmd_rollforward.sh
@@ -11,14 +11,14 @@ store_oids_a=$(cat store-addresses | jq '.git_annex.addr')
 #oid_a_1=$(echo "$store_oids_a" | cut -d ' ' -f 1 | sed 's/"//g')
 #oid_a_2=$(echo "$store_oids_a" | cut -d ' ' -f 2 | sed 's/"//g')
 
-git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout
+git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout > /dev/null 2>&1
 
 store_oids_b=$(cat store-addresses | jq '.git_annex.addr')
 if [ "$store_oids_a" != "$store_oids_b" ]; then
     cd files/
 
     # We can't rollforward the git-annex branch also, but if we do make the changes permanent, we can.
-    git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout
+    git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse HEAD) | tail -n1 | xargs git checkout > /dev/null 2>&1
 
 fi
 #echo "$oid_1"
