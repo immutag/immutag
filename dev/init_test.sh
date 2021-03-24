a1902220ef761b7a4f467e280ed59a3c310083f7
7db9a <repo.7db9a@gmail.com>
2021-03-23 21:42:04 -0400

    tests: refactor test with high-level imt create.

--- dev/init_test.sh
+++ dev/init_test.sh
@@ -6,11 +6,11 @@
 #}
 
 # To use command with cat
-# imt_init "$(eval cat $mnemonic)"
+# imt create "$(eval cat $mnemonic)"
 # without the quotations, it fails.
 
 testEquality() {
-    imt_init main "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
+    imt create main "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
     result_list=$(eval ls /root/immutag/main/file-list.txt)
     result_entropy=$(eval cat /root/immutag/main/wallet-info | jq '.entropy_bits')
     result_config=$(eval wc -l < $HOME/.immutag_config)
@@ -18,7 +18,7 @@ testEquality() {
     assertEquals "256" "$result_entropy"
     assertEquals "1" "$result_config"
 
-    imt_init media "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
+    imt create media "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
     result_list=$(eval ls /root/immutag/media/file-list.txt)
     result_entropy=$(eval cat /root/immutag/media/wallet-info | jq '.entropy_bits')
     result_config=$(eval wc -l < $HOME/.immutag_config)
@@ -26,7 +26,7 @@ testEquality() {
     assertEquals "256" "$result_entropy"
     assertEquals "2" "$result_config"
 
-    imt_init main "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
+    imt create main "lottery shop below speed oak blur wet onion change light bonus liquid life fat reflect cotton mass chest crowd brief skin major evidence bamboo"
     result_config=$(eval wc -l < $HOME/.immutag_config)
     assertEquals "2" "$result_config"
 
