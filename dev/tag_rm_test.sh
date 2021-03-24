68a940014df5c160d74c506f7848db1bfe31f314
7db9a <repo.7db9a@gmail.com>
2021-03-23 22:36:09 -0400

    tests: refactor test with high-level imt tag-rm.

--- dev/tag_rm_test.sh
+++ dev/tag_rm_test.sh
@@ -12,7 +12,7 @@ testEquality() {
     imt add --no-default-name "$name" foo.txt tag1 tag2 tag3
     imt add --no-default-name "$name" bar.txt tag1 tag2 tag3
 
-    echo "17nZVxSmir9moZQSAwrPd7r7rRRdNqovGr" | imt_tag_rm "$name"
+    imt rm-tags "$name" "17nZVxSmir9moZQSAwrPd7r7rRRdNqovGr"
 
     cd "$immutag_path"
     cd "$name"
