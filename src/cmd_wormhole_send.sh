2e9c64107bba6ed850b25c15787cae306923f80d
7db9a <repo.7db9a@gmail.com>
2021-04-16 11:37:33 -0400

    Saving progress: use rsync for tmp file before send.

--- src/cmd_wormhole_send.sh
+++ src/cmd_wormhole_send.sh
@@ -4,18 +4,9 @@ immutag_path="$HOME/immutag"
 name="$1"
 
 cd "$immutag_path" || exit
-cd "$name" || exit
+rsync -a --copy-links "$name" /tmp/ \
+--exclude wallet-info \
+--exclude .git \
+--exclude files/.git \
 
-mkdir /tmp/immutag-git
-cp -r .git /tmp/immutag-git/.git
-
-cd /tmp/immutag-git
-git reset HEAD --hard
-
-git checkout --orphan temp
-git add -A
-git commit -am "update"
-git branch -D master && git branch -m master
-
-# Send file-list git
-wormhole send .git
+wormhole send "$name"
