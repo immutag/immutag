14e5ac20a804a897aadce56dc934825a6adb86ee
7db9a <repo.7db9a@gmail.com>
2021-04-16 10:28:43 -0400

    Saving progress: use tmp to send git.

--- src/cmd_wormhole_send.sh
+++ src/cmd_wormhole_send.sh
@@ -6,5 +6,16 @@ name="$1"
 cd "$immutag_path" || exit
 cd "$name" || exit
 
+mkdir /tmp/immutag-git
+cp -r .git /tmp/immutag-git/.git
+git reset HEAD --hard
+
+cd /tmp/immutag-git
+
+git checkout --orphan temp
+git add -A
+git commit -am "update"
+git branch -D master && git branch -m master
+
 # Send file-list git
 wormhole send .git
