58f032a4505054fd90ec92a9e950737c0978267a
7db9a <repo.7db9a@gmail.com>
2021-05-07 17:03:00 -0400

    fix: loop until store definitely updated.

--- src/_imt_write_store_addrs.sh
+++ src/_imt_write_store_addrs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 name="$1"
 
 addresses_type="$2"
@@ -17,10 +19,40 @@ config_path=$immutag_path/$name/"$addresses_type"-addresses
 ls $config_path
 config_exists=$(eval echo $?)
 
-if [ "$config_exists" = "2" ]; then
-    # Create config
-    echo '{ "sha256": { "version": "arg", "addr": "arg" }, "ipfs": { "version": "arg", "addr": "arg"}, "git_annex": { "version": "arg", "addr": "arg"} }' > $config_path
-fi
+while [ $(cat "$config_path") = "" ]
+do
+     #if [ "$config_exists" = "2" ]; then
+         # Create config
+         echo '{ "sha256": { "version": "arg", "addr": "arg" }, "ipfs": { "version": "arg", "addr": "arg"}, "git_annex": { "version": "arg", "addr": "arg"} }' > $config_path
+     #fi
+
+      echo "name" >> debug
+      echo "$name" >> debug
+      echo "addresses_type" >> debug
+      echo "$addresses_type" >> debug
+      echo "sha_version" >> debug
+      echo "$sha_version" >> debug
+      echo "sha_addr" >> debug
+      echo "$sha_addr" >> debug
+      echo "ipfs_version" >> debug
+      echo "$ipfs_version" >> debug
+      echo "ipfs_addr" >> debug
+      echo "$ipfs_addr" >> debug
+      echo "gitannex_version" >> debug
+      echo "$gitannex_version" >> debug
+      echo "gitannex_addr" >> debug
+      echo "$gitannex_addr" >> debug
+      echo "immutag_path" >> debug
+      echo "$immutag_path" >> debug
+      echo "config_path" >> debug
+      echo "$config_path" >> debug
+
+
+    if [ $(cat "$config_path") != "" ]; then
+	    break
+
+    fi
+done
 
 cat $config_path \
 | jq --arg sha_version "$sha_version" '.sha256.version = $sha_version' \
@@ -30,3 +62,23 @@ cat $config_path \
 | jq --arg gitannex_version "$gitannex_version" '.git_annex.version = $gitannex_version' \
 | jq --unbuffered --arg gitannex_addr "$gitannex_addr" '.git_annex.addr = $gitannex_addr' \
 > $config_path
+
+
+while [ $(cat "$config_path") = "" ]
+do
+
+
+    cat $config_path \
+    | jq --arg sha_version "$sha_version" '.sha256.version = $sha_version' \
+    | jq --arg sha_addr "$sha_addr" '.sha256.addr = $sha_addr' \
+    | jq --arg ipfs_version "$ipfs_version" '.ipfs.version = $ipfs_version' \
+    | jq --arg ipfs_addr "$ipfs_addr" '.ipfs.addr = $ipfs_addr' \
+    | jq --arg gitannex_version "$gitannex_version" '.git_annex.version = $gitannex_version' \
+    | jq --unbuffered --arg gitannex_addr "$gitannex_addr" '.git_annex.addr = $gitannex_addr' \
+    > $config_path
+
+    if [ $(cat "$config_path") != "" ]; then
+	    break
+
+    fi
+done
