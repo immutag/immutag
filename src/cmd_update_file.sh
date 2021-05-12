3d7b10199b53b6a3a1d04e3a27515e9b302cccb4
7db9a <repo.7db9a@gmail.com>
2021-05-12 18:30:27 -0400

    improvement: fail if updating file version that already is current.

--- src/cmd_update_file.sh
+++ src/cmd_update_file.sh
@@ -6,6 +6,13 @@ file="$3"
 
 immutag_path="$HOME/immutag"
 
-mv $file "$immutag_path"/"$name"/files/"$addr"
-echo "update file: $file"
-_imt_commit "$name"
+# Compares files and gives appropriate exit_update code.
+cmp --silent "$file" "$immutag_path"/"$name"/files/"$addr" && exit_update="2" || exit_update="0"
+
+if [ "$exit_update" = "0" ];then
+    mv $file "$immutag_path"/"$name"/files/"$addr"
+    echo "update file: $file"
+    _imt_commit "$name"
+else
+    echo "Update failed: already the current file version."
+fi
