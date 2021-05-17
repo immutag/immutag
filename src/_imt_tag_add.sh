d23d89bd6671289edb33b1b939549a5092f71e1b
7db9a <repo.7db9a@gmail.com>
2021-05-17 10:20:44 -0400

    improvement: add tag command output includes current and new tags.

--- src/_imt_tag_add.sh
+++ src/_imt_tag_add.sh
@@ -7,8 +7,8 @@ tags=$(echo "$input" | cut -d " " -f 2-)
 
 addr="$(</dev/stdin)"
 line="$(eval rg $addr $filelist)"
-echo "$line"
-#line_old_tags="$(eval echo "$line" | cut -d ' ' -f 2-)"
-#echo "$line_old_tags $tags"
+#echo "$line"
+line_old_tags="$(eval echo "$line" | cut -d ' ' -f 2-)"
+echo "$line_old_tags $tags"
 
 sed -i "s/$line/$line $tags/" $filelist
