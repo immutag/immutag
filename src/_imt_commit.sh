785e352965356488f24ce96040cba463fc856ae6
7db9a <repo.7db9a@gmail.com>
2021-05-19 12:24:37 -0400

    fix: make sure store addresses is written.

--- src/_imt_commit.sh
+++ src/_imt_commit.sh
@@ -8,6 +8,29 @@ cd "$name" || exit
 
 imt_commit_store > /dev/null 2>&1
 _imt_update_list_store_hashes "$name" > /dev/null 2>&1
+config_path=$immutag_path/$name/store-addresses
+exists="1"
+
+if [ -s "$config_path" ];then
+    # exists
+    exists="0"
+else
+    # doesn't exist
+    exists="1"
+fi
+
+while [ $exists == "1" ]
+do
+   git restore store-addresses > /dev/null 2>&1
+   _imt_update_list_store_hashes "$name" > /dev/null 2>&1
+    if [ -s "$config_path" ];then
+        # exists
+        exists="0"
+    else
+        # doesn't exist
+        exists="1"
+    fi
+done
 imt_commit_list > /dev/null 2>&1
 # Only does so if checked out from a rollback or rollforward.
 _imt_exit_checkout > /dev/null 2>&1
--- src/_imt_write_store_addrs.sh
+++ src/_imt_write_store_addrs.sh
@@ -14,50 +14,20 @@ gitannex_version="$7"
 gitannex_addr="$8"
 
 immutag_path="$HOME/immutag"
-config_path=$immutag_path/$name/"$addresses_type"-addresses
+config_path=$immutag_path/$name/store-addresses
 
-ls $config_path
-config_exists=$(eval echo $?)
+exists="no"
 
-if [ "$config_exists" = "2" ]; then
+if [ -s "$config_path" ];then
+	exists="yes"
+else
+	exists="no"
+fi
+
+if [ "$exists" == "no" ]; then
    # Create config
    echo '{ "sha256": { "version": "arg", "addr": "arg" }, "ipfs": { "version": "arg", "addr": "arg"}, "git_annex": { "version": "arg", "addr": "arg"} }' > $config_path
 fi
-
-while [ $(cat "$config_path") = "" ]
-do
-     #if [ "$config_exists" = "2" ]; then
-         # Create config
-         echo '{ "sha256": { "version": "arg", "addr": "arg" }, "ipfs": { "version": "arg", "addr": "arg"}, "git_annex": { "version": "arg", "addr": "arg"} }' > $config_path
-     #fi
-
-      echo "name" >> debug
-      echo "$name" >> debug
-      echo "addresses_type" >> debug
-      echo "$addresses_type" >> debug
-      echo "sha_version" >> debug
-      echo "$sha_version" >> debug
-      echo "sha_addr" >> debug
-      echo "$sha_addr" >> debug
-      echo "ipfs_version" >> debug
-      echo "$ipfs_version" >> debug
-      echo "ipfs_addr" >> debug
-      echo "$ipfs_addr" >> debug
-      echo "gitannex_version" >> debug
-      echo "$gitannex_version" >> debug
-      echo "gitannex_addr" >> debug
-      echo "$gitannex_addr" >> debug
-      echo "immutag_path" >> debug
-      echo "$immutag_path" >> debug
-      echo "config_path" >> debug
-      echo "$config_path" >> debug
-
-
-    if [ $(cat "$config_path") != "" ]; then
-	    break
-
-    fi
-done
 
 cat $config_path \
 | jq --arg sha_version "$sha_version" '.sha256.version = $sha_version' \
@@ -67,23 +37,3 @@ cat $config_path \
 | jq --arg gitannex_version "$gitannex_version" '.git_annex.version = $gitannex_version' \
 | jq --unbuffered --arg gitannex_addr "$gitannex_addr" '.git_annex.addr = $gitannex_addr' \
 > $config_path
-
-
-while [ $(cat "$config_path") = "" ]
-do
-
-
-    cat $config_path \
-    | jq --arg sha_version "$sha_version" '.sha256.version = $sha_version' \
-    | jq --arg sha_addr "$sha_addr" '.sha256.addr = $sha_addr' \
-    | jq --arg ipfs_version "$ipfs_version" '.ipfs.version = $ipfs_version' \
-    | jq --arg ipfs_addr "$ipfs_addr" '.ipfs.addr = $ipfs_addr' \
-    | jq --arg gitannex_version "$gitannex_version" '.git_annex.version = $gitannex_version' \
-    | jq --unbuffered --arg gitannex_addr "$gitannex_addr" '.git_annex.addr = $gitannex_addr' \
-    > $config_path
-
-    if [ $(cat "$config_path") != "" ]; then
-	    break
-
-    fi
-done
