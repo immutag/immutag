14f7caa02c5e92e6b900edb709441b1d35876d6e
7db9a <noreply@yourdomain.com>
2026-06-29 18:40:45 -0600

    fix: correct teardown test to use variable instead of literal string

--- tests/teardown_test.sh
+++ tests/teardown_test.sh
@@ -2,8 +2,8 @@
 
 testEquality() {
     immutag_path="$HOME/immutag"
-    rm -r immutag_path
-    ls immutag_path
+    rm -r "$immutag_path"
+    ls "$immutag_path"
     # Get exit codeo of ls: 2 means dir doesn't exist.
     result_exit_code=$(echo $?)
     assertEquals "2" "$result_exit_code"
