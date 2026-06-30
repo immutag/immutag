612a543228aeace91e7c915e8e3fb475d2ce75c0
7db9a <noreply@yourdomain.com>
2026-06-29 18:54:04 -0600

    fix: recreate stage directory in teardown to preserve container WORKDIR

--- tests/teardown_test.sh
+++ tests/teardown_test.sh
@@ -7,6 +7,7 @@ testEquality() {
     # Get exit codeo of ls: 2 means dir doesn't exist.
     result_exit_code=$(echo $?)
     assertEquals "2" "$result_exit_code"
+    mkdir -p "$immutag_path/stage"
 }
 
 . shunit2
